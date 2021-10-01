4aa0d2b83d5b970625453603ad6315b0516ca47a
IdONTKnowCHEK <chak12346789@gmail.com>
2021-10-01 16:07:04 +0800

    gg

--- lib/flutter_login.dart
+++ lib/flutter_login.dart
@@ -603,7 +603,7 @@ class _FlutterLoginState extends State<FlutterLogin>
     final deviceSize = MediaQuery.of(context).size;
     const headerMargin = 15;
     const cardInitialHeight = 300;
-    final cardTopPosition = deviceSize.height / 2 - cardInitialHeight / 2;
+    final cardTopPosition = deviceSize.height / 2 - cardInitialHeight / 2 - 20;
     final headerHeight = cardTopPosition - headerMargin;
     final userValidator =
         widget.userValidator ?? FlutterLogin.defaultEmailValidator;
